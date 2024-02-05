4c1aebd170cfde9c41e0c7468677310784864df1
VictorJVA <vjvilladia@eafit.edu.co>
2024-02-04 22:58:48 -0500

    Song_Artists_Info

--- Spotify_API/theAPI.py
+++ Spotify_API/theAPI.py
@@ -1,19 +1,31 @@
+#Importamos librerías de Spotify(Spotify for developers)
+
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+#Guardamos en una variable nuestro client_id y client_secret
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="4007b4a4055d44478ac95dba0bfb28fd",
                                                            client_secret="02d214197de54bf6bf0deb21b8f19f9f"))
 
 
-song_id = "7xPhfUan2yNtyFG0cUWkt8"
+#Ejemplo canción
+song_id = "0R6NfOiLzLj4O5VbYSJAjf"
 song_audio_features = sp.audio_features(song_id)
-
 song_info = sp.track(song_id)
 
+artist_id = "3Me35AWHCGqW4sZ7bWWJt1"
+artist_info = sp.artist(artist_id)
+artist_more_info = sp.artist_related_artists(artist_id)
+
+playlist_id = "37i9dQZEVXbMDoHDwVN2tF"
+playlist_info = sp.playlist_tracks(playlist_id)
+
+print("Información artista y algunas cosas de la canción\n")
 print(song_info)
+print("Datos del artista\n")
+print(artist_info)
+print("Datos análisis de la canción\n")
 print(song_audio_features)
-
-
-
-
-
+print("Playlist Info\n")
+print(artist_more_info)
+#print(playlist_info)
